ba872c78da433206c26756d68e92f8823f061fb9
Assadmus47 <assadkac47@gmail.com>
2026-03-10 23:49:48 +0100

    feat:(maze.py) create class maze and has_wall method

--- src/maze.py
+++ src/maze.py
@@ -1,8 +1,50 @@
-NORTH = ...
-EAST  = ...
-SOUTH = ...
-WEST  = ...
+NORTH = 1
+EAST  = 2
+SOUTH = 4
+WEST  = 8
+
+OPPOSITE = {
+    NORTH: SOUTH,
+    EAST: WEST,
+    WEST: EAST,
+    SOUTH: NORTH
+}
+
+DIRECTIONS = {
+    NORTH: (0, -1),
+    SOUTH: (0, 1),
+    EAST: (1, 0),
+    WEST: (-1, 0),
+}
 
 class Maze:
-    def __init__(self):
-        pass
+    """Represents the maze grid.
+
+    Attributes:
+        width: The width of the maze in cells.
+        height: The height of the maze in cells.
+        grid: 2D list containing wall data for each cell.
+    """
+    def __init__(self, width: int, height: int) -> None:
+        """Initializes the maze grid with all walls closed.
+
+        Args:
+            width: The number of cells horizontally.
+            height: The number of cells vertically.
+        """
+        self.width: int = width
+        self.height: int = height
+        self.grid: list[list[int]] = [[0xF for _ in range(width)] for _ in range(height)]
+
+    def has_wall(self, x: int, y: int, direction: int) -> bool:
+        """check if there is a wall in the direction given as a parameter.
+
+        Args:
+            x: column.
+            y: row.
+            direction: the direction that we want if there is a wall on it or no.
+
+        Returns:
+            returns bool result True if there is a wall , False if there isnt.
+        """
+        return bool(self.grid[y][x] & direction)
