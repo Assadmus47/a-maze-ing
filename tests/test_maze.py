147d7551896d62266c620e185f65c0ebdc14edbf
Assadmus47 <assadkac47@gmail.com>
2026-03-12 05:10:02 +0100

    test (test_maze.py)testting all methods of maze

--- tests/test_maze.py
+++ tests/test_maze.py
@@ -8,11 +8,14 @@ from src.maze import Maze
 
 def test():
     maze = Maze(20, 15)
-    maze.generate(99, True)
     maze.place_42_pattern()
+    maze.generate(99, True)
     for i in range (15):
         for j in range(20):
-            print(maze.grid[i][j], end="")
+            if maze.grid[i][j] == 15:
+                print("o", end="")
+            else:
+                print(" ", end="")
         print()
 
 
